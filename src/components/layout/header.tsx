ca8786a9003babca63ea391f7329d889a6191604
Sanele Krakra <sanele23@live.com>
2026-03-30 08:00:56 +0200

    fix: logo fix

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -28,7 +28,7 @@ export default function Header() {
         {/* Logo */}
         <Link href="/" className="flex items-center gap-2">
           <Image
-            src="/cebisa-logo.jpeg"
+            src="/cebisa-logo.png"
             alt="Cebisa Memorial Logo"
             width={60}
             height={60}
